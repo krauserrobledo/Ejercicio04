b295597935f932bdf00b12855dc9fc08c36d140a
krauserrobledo <krauser.robledo@gmail.com>
2024-05-02 20:55:03 +0200

    un mensaje

--- Proyecto1/src/paquete1/Main.java
+++ Proyecto1/src/paquete1/Main.java
@@ -4,8 +4,7 @@ public class Main {
 	
 	public static void main (String [] args){
 		System.out.println("Estamos haciendo control de versiones con Git");
-		System.out.println("Estamos haciendo otro control de versiones con Git");
-		System.out.println("Estamos haciendo uno mas control de versiones con Git");
+
 	}
 
 }
